830be5e0ada1039cf1ce8f5f1042edcff3094733
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2022-11-08 19:49:38 -0300

    Atualizacao de configuracao de parametrizacao do angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
-  endereco: 'http://localhost:',
-  porta: '8080',
+  // endereco: 'http://localhost:',
+  // porta: '8080',
+  endereco: 'http://191.101.70.14',
+  porta: ':8080',
   id: 0,
   username: '',
   senha: '',
